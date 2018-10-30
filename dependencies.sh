bbdfd573c60d0313d27526d057d674b70716767b
Ragen Patel <ragenpatel@live.com>
2018-10-30 17:48:17 -0400

    Updated dependencies file

--- dependencies.sh
+++ dependencies.sh
@@ -20,7 +20,7 @@ tar -xzf node-v8.9.0-linux-armv6l.tar.gz
 sleep 10
 echo ""
 echo "Installing..."
-cd node-v6.11.1-linux-armv6l/
+cd node-v8.9.0-linux-armv6l/
 sudo cp -R * /usr/local/
 echo "----\nVersion Info: Node, npm"
 node -v
@@ -28,7 +28,11 @@ npm -v
 echo ""
 echo "Node/npm installed successfully!"
 echo ""
-echo ""
+echo "removing downloaded node files"
+
+cd ~/YungBot/LeagueDiscord/
+sudo rm -r node-v8.9.0*
+echo "Files removed"
 
 echo "Installing foreverjs"
 
@@ -36,54 +40,13 @@ sudo npm install forever -g
 
 echo "...Installed!"
 
-
-echo "installing discord.js"
-
-sudo npm install --save discord.js
-
-echo "...Installed!"
-
-echo ""
-echo "Installing request"
-
-sudo npm install request --save
-
-echo "Installed request\n===================================="
-echo "Installing async"
-
-sudo npm install --save async
-
-echo "Installed async\n==================================="
-echo "Installing GoogleURL"
-
-sudo npm install googleapis --save
-
-echo "Installed GoogleURL"
-echo ""
-
-echo "Installing bitly API"
-
-sudo npm install bitly --save
-
-echo "Installed!"
-echo ""
-
-echo "Installing python-shell"
-
-sudo npm install python-shell --save
-
-echo "Python-Shell Installed!"
+echo "npm install..."
+sudo npm install
 
 echo "Installing Python dependencies"
 echo ""
 echo "Installing Pillow"
 pip install Pillow
 echo ""
-
-echo "Installing Dotenv"
-sudo npm install dotenv
-echo ""
-echo "Dotenv Installed!"
-
 
 echo "Installed all packages"
