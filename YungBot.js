2629b427609bc96bdaf3147c53fb549080b9dbfe
Pi <pi@pi.com>
2021-08-10 13:48:01 -0400

    removed stuff

--- YungBot.js
+++ YungBot.js
@@ -3,13 +3,10 @@ var request = require("request");
 var emotes = require("./classes/emotes.js");
 var sentimentAnalysis = require("./classes/sentimentAnalysis.js");
 var emojis = require('./classes/addEmojis.js');
-var league = require('./classes/league.js');
 var db = require('./classes/db/postgresDB.js');
-var reddit = require('./classes/ice_reddit.js');
 var esp32 = require('./classes/IoT/esp32.js');
 
 const spawn = require("child_process").spawn;
-const { BitlyClient } = require('bitly');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
@@ -18,74 +15,20 @@ require('dotenv').config();
 //File reader of .txt so I can upload this project to GitHub without disclosing APIs
 var fs = require('fs')
 
-//var functions = require('../controllers/functions.js');
-
 //USE forever -w yungbot.js !!!!!!!
 //Make functions
 
 var line;
 var checkID;
 
-/*
-fs.readFile('APIs.txt', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log("Data is \n" + data);
-  console.log(dataJson = data.split(" "));
-  discordTokAPI = dataJson[0];
-  console.log("discordTokAPI " + discordTokAPI);
-}); */
-
-//console.log("Outside function: " + data);
-
 //set API keys here
 var discordTok = process.env.DISCORD_API_TOKEN;
-var api_key = process.env.RIOT_API;
-var bitly_key = process.env.BITLY_API;
-
-const bitly = new BitlyClient(bitly_key, {});
-
-
-//======================================================================================================================
-// USE THIS WHEN A NEW CHAMPION IS RELEASED TO UPDATE CHAMPIONS LIST----------------------------------------------------
-//======================================================================================================================
-//  https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=<key>
-//  /lol/static-data/v3/champions
-/* var URL = 'https://na1.api.riotgames.com/lol/static-data/v3/champions?api_key='+api_key;
-request(URL, function(err, response, body){
-    var json = JSON.parse(body);
-    var saveData = JSON.stringify(json);
-    console.log(json);
-
-    fs.writeFile("champions.txt", saveData, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-    });
-
-}); */
-
-
 
 var region = 'na';
 var data = {};
 
 var bot = new Client();
 
-reddit.redditCheck(bot, 300000)
-
-// bot.on('ready', () => {
-//     function checkerRandom() {
-//         var min = 60,
-//           max = 300;
-//         var rand = Math.floor(Math.random() * (max - min + 1) + min); //Generate Random number between 5 - 10
-//         exec('/home/pi/InventoryChecker/runChecker.sh')
-//         setTimeout(checkerRandom, rand * 1000);
-//     }
-//     // setInterval( exec('source /home/pi/InventoryChecker/runChecker.sh'); , 60000)
-//     checkerRandom()
-// })
 
 //Start of bot
 bot.on("message", function(message){
@@ -125,7 +68,6 @@ bot.on("message", function(message){
             .addField("?addemote emotename url/remove", "used to add an emote via the url or remove the emote")
             .addField("~summonerName", "Get League user info")
             .addField("?ingame summonerName", "Get ingame time for a user")
-            .addField("?bitly <link>", "shorten a url")
             .addField("?addemoji url name", "adds emote to discord emojis list (native)")
             .addField("?emotes", "Gets a list of all the current available emotes")
             .addField("?sqlRemove emoteName", "Remove an emote from the Postgres Database")
@@ -140,19 +82,6 @@ bot.on("message", function(message){
     //test second .js file
     // var checkEmote = false
     checkEmote = emotes.checkForEmotes(input, message);
-
-    // if(checkEmote[1] == true) {
-    //     console.log()
-    // } else if (checkEmote[0] != false) {
-    //     message.channel.send({files: ['./emotesImages/'+checkEmote+'.png']});
-    // }
-
-
-
-    //help command
-    // if(input == "--h" || input == "?help"){
-    //     help();
-    // }
 
     if(input.indexOf("#region") == 0){
         var arr = input.split(" ");
@@ -175,26 +104,6 @@ bot.on("message", function(message){
         message.channel.send("https://i.ytimg.com/vi/vN7EoPlxQsQ/hqdefault.jpg");
     }
 
-    // if(input.indexOf(" na ") >= 0){
-    //   var randNum = Math.floor(Math.random() * 100);
-    //   console.log("random num: " + randNum);
-    //   if(randNum < 20) {
-    //     message.channel.send("C9 3-0 out of groups Poggers");
-    //   }
-    // } else if (input.indexOf("na") == 0 && input.length == 2){
-    //   var randNum = Math.floor(Math.random() * 100);
-    //   console.log("random num: " + randNum);
-    //   if(randNum < 30) {
-    //     message.channel.send("LETS GO NA Pog");
-    //   }
-    // } else if (input.indexOf(" na")>=0 && input.length-2 == input.indexOf("na")){
-    //   var randNum = Math.floor(Math.random() * 100);
-    //   console.log("random num: " + randNum);
-    //   if(randNum < 40) {
-    //     message.channel.send("LETS GO NA Poggers");
-    //   }
-    // }
-
     if(input.indexOf("?emotes") == 0 || input.indexOf("!emotes") == 0) {
         const embed = new RichEmbed()
         message.channel.send(embed
@@ -202,25 +111,6 @@ bot.on("message", function(message){
             .setTitle("YungBot GIF Emotes")
             .setURL("https://yungbotemotes.github.io/"))
             .catch(console.error);
-            // .addField("Emote List", emoteList))
-
-        // var path = './emotesImages';
- 
-        // fs.readdir(path, function(err, items) {
-        //     console.log(items);
-        //     var emoteList = ""
-        //     const embed = new RichEmbed();
-
-        //     for (var i=0; i<10; i++) {
-        //         if (items[i].includes(".png")) {
-        //             emoteList += items[i].replace(".png", "") + "\n";
-        //         }
-        //     }
-        // });
-    }
-
-    if(input.includes("ice_poseidon") && input.includes('reddit.com')) {
-        reddit.dataFromURL(input, message)
     }
 
     if((input.includes("c9") || input.includes(" na ") || input.includes("sneaky"))) {
@@ -229,59 +119,6 @@ bot.on("message", function(message){
 
     if(input.indexOf("emojis") == 0) {
         emojis.addEmojis(input, message);
-    }
-
-    // if (message.author.id != '197948432961241089' && input.indexOf('?updatedb') == 0) {
-    //     console.log("updating database!")
-    //     db.updateChampions();
-    //     db.updateItems();
-    // }
-
-    // if (message.author.id != '197948432961241089' && input == '?createtable') {
-    //     console.log("CREATED TABLE ~~~~~~~~~~~~~~~~")
-    //     db.createTable();
-    // }
-
-    if (input.indexOf("checkID: ") == 0) {
-        league.checkStats(input, message);
-    }
-
-    if (input.indexOf("?matchup ") == 0) {
-        league.getMatchup(input, message);
-    }
-
-    //IMPLEMENT GETTING CUSTOM TIMES FOR ANY PERSON.. MAYBE EVEN REGIONS
-    //get ongoing game data. i.e current game length
-    if(input.indexOf("?ingame") == 0){
-        const embed = new RichEmbed()
-        league.ingame(input, message, embed);
-    }
-
-    if (input.charAt(0) == '~' && input.indexOf("ingame")==1){
-        var arr = input.split(" ");
-        if(arr.length != 2){
-            message.channel.send("`Invalid format. Use: '~ ingame *SUMMONERNAME*`");
-        }else{
-            console.log("Finding sID for "+ arr[1]);
-            message.channel.send("!lol match na " + arr[1]);
-        }
-    }
-
-    if(input.charAt(0) == '~') {
-        summonerInfo(input, false);
-    }
-
-    if(input.indexOf("?bitly") == 0) {
-        var input_arr = input.split(" ");
-        var url = input_arr[1];
-
-        bitly.shorten(url)
-        .then(function(result) {
-            message.channel.send(result['url']);
-        })
-        .catch(function(error) {
-            console.error(error);
-        });
     }
 
     // help command
@@ -312,46 +149,6 @@ bot.on("message", function(message){
         message.channel.send("kappabot logs", { files: ["/home/pi/.forever/kappabot.log"] });
     }
 
-    if(message.content.startsWith('?redditsub')) {
-        var input_arr = input.split(" ");
-        var subReddit = input_arr[1];
-
-         fs.appendFile('.env', ','+subReddit, function (err) {
-            if (err) {
-                message.channel.send("Error" + err)
-                throw err;
-            }
-            message.channel.send("Saved!")
-        });
-    }
-
-    if(message.content.startsWith('?redditkeys')) {
-        fs.readFile(".env", 'utf8', function (err,data) {
-            const currentKeys = data.split("REDDIT_KEYWORDS=")
-            bot.channels.get("198297756295495681").send(currentKeys[1])
-        });
-    }
-
-    if(message.content.startsWith('?redditdelete')) {
-        var input_arr = input.split(" ");
-        var stringToDelete = input_arr[1];
-
-        fs.readFile(".env", 'utf8', function (err,data) {
-            var formatted = data.replace(","+stringToDelete, '');
-
-            fs.writeFile(".env", formatted, 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
-        });
-
-    }
-
-    // MARK: SQL Execution
-    if (message.content.startsWith('!sql')) {
-        var query = message.content.substr(5);
-        db.executeCustomQuery(query, message);
-    }
-
     if (message.content.startsWith('!getimage') && process.env.SUDO_USER == message.author.id) {
         console.log("userId: " + message.author.id);
         esp32.getCapture(bot);
@@ -379,12 +176,6 @@ bot.on("message", function(message){
         console.log(">>>> emote usage calling db")
         db.getTop10UsedEmotes(message);
     }
-
-    // Sending image from url
-    // if(input.indexOf('test')==0) {
-    //     message.channel.send("", {files: ['https://cdn.frankerfacez.com/7390d699a3362a0f5a7fe4ca3c643b24.PNG']})
-    // }
-
 });
 
 bot.login("Bot "+ discordTok);
