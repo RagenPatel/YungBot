ef44c8026b6f9f73305d3efd0bc11f1cf2629725
Pi <pi@pi.com>
2021-10-11 23:40:25 -0400

    added botbot logging

--- start_bot.sh
+++ start_bot.sh
@@ -8,9 +8,11 @@ source venv/bin/activate
 python3 main.py &
 python3 twitch_chat.py &
 
+CURRENT_DATE="botbot-"`date +"%Y-%m-%d-%T"`".log"
+
 deactivate
 cd ~/Yashas-DiscordBot/discordbot
 rm -rf *.mp3
 source venv/bin/activate
-python3 botbot.py > botbot.log &
+python3 botbot.py > ${CURRENT_DATE} &
 
