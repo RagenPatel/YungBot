2e29835791b6d73ec4a41a0c49456f24551ba8e3
Ragen <ragenpatel@live.com>
2022-09-03 20:46:01 -0400

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -10,9 +10,9 @@ python3 twitch_chat.py &
 
 CURRENT_DATE="botbot-"`date +"%Y-%m-%d-%T"`".log"
 
-deactivate
-cd ~/Yashas-DiscordBot/discordbot
-rm -rf *.mp3
-source venv/bin/activate
-python3 botbot.py > ${CURRENT_DATE} &
+# deactivate
+# cd ~/Yashas-DiscordBot/discordbot
+# rm -rf *.mp3
+# source venv/bin/activate
+# python3 botbot.py > ${CURRENT_DATE} &
 
