5fc9197f2a9ba493631df3b275f4ff95a6bb4f7d
Ragen <ragenpatel@live.com>
2024-05-30 00:15:06 -0400

    Dockerizing after this

--- start_bot.sh
+++ start_bot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash/
-cd /home/pi/dev/YungBot/
+cd /root/yungbot 
 cd v2
 
 source venv/bin/activate
 python3 main.py &
-python3 twitch_chat.py &
-python3 track_reddit.py &
+# python3 twitch_chat.py &
+# python3 track_reddit.py &
 
 CURRENT_DATE="botbot-"`date +"%Y-%m-%d-%T"`".log"
 
